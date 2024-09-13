d8c5752a753df54c99e2839899c32b5eb5dc608e
Ayomide, M. Jayeola <162918551+ZenellesTECH@users.noreply.github.com>
2024-09-13 19:50:34 +0100

    Add files via upload

---
+++ checking players name.py
@@ -0,0 +1,8 @@
+registerd_players=["Bal","Rick","Steve"]
+player1=input("player1-please enter your name")
+while player1 not registered_players:
+    player1=input("player1- please enter a valid name")
+player2=input ("player2- please enter your name")
+while player2 not registered_players:
+   player2=input("player2- please enter a valid name") 
+    
