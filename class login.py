0155d2d3a00fb42362e122353f0dcee2adcda72f
Ayomide, M. Jayeola <162918551+ZenellesTECH@users.noreply.github.com>
2024-09-13 20:00:04 +0100

    Add files via upload

---
+++ class login.py
@@ -0,0 +1,10 @@
+print("welcome to gmail")
+print("please login")
+
+username='none'
+password='none'
+while username!="akira":
+    username=input("please enter your username")
+while password!="my password":
+    password=input("please enter your password")
+print("welcome back", username)
